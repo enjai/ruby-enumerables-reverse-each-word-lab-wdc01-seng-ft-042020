0d85a18c4113c55b960e94c0a6aaa6f4ad0b1205
Injae Lee <ilee.injae@gmail.com>
2020-04-10 19:47:41 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -7,3 +7,10 @@ def reverse_each_word(string)
   new_array.join(" ")
 end
 
+def reverse_each_word(string)
+  string.split.collect {|word| word.reverse}.join(" ")
+end
+
+def reverse_each_word(string)
+  string.reverse.split.reverse.join(" ")
+end
