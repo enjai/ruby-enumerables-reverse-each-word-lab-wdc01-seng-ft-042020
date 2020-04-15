9b945930466e7c27709f6647a3df608c39b66955
Injae Lee <ilee.injae@gmail.com>
2020-04-15 19:57:00 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -11,6 +11,6 @@ def reverse_each_word(string)
   string.split.collect {|word| word.reverse}.join(" ")
 end
 
-def reverse_each_word(string)
-  string.reverse.split.reverse.join(" ")
-end
+#def reverse_each_word(string)
+ # string.reverse.split.reverse.join(" ")
+#end
